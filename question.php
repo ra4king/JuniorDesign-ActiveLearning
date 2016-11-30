c1a62cc0a405d894fe203878a4f2b2685e393efb
Shazambom <mrderp14@gmail.com>
2016-11-29 19:31:25 -0500

    Created questionCreator.html which is a form to submit questions to the server.

---
+++ question.php
@@ -0,0 +1,11 @@
+<html>
+	<body>
+		Question submitted!
+		<?php echo $_POST["question"]; ?><br>
+		<?php echo $_POST["answer1"]; ?><br>
+		<?php echo $_POST["answer2"]; ?><br>
+		<?php echo $_POST["answer3"]; ?><br>
+		<?php echo $_POST["answer4"]; ?><br>
+		<?php echo $_POST["imageInput"]; ?><br>
+	</body>
+</html>
