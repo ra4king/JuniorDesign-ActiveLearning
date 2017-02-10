b7c9b53f43a8b23a4f43144acec79df90fb09891
Chad Turner <chadwickpturner@gmail.com>
2017-02-10 13:00:47 -0500

    Added students page base. Added get students method in database. Added home button and selected status.

--- database.js
+++ database.js
@@ -11,6 +11,7 @@ module.exports = {
     delete_question: delete_question,
     get_question_by_id: get_question_by_id,
     get_questions: get_questions,
+    get_students: get_students,
 
     create_quiz: create_quiz,
     update_quiz: update_quiz,
@@ -275,6 +276,29 @@ function get_questions(callback) {
         });
 
         callback(null, cleaned);
+    });
+}
+
+function get_students(callback) {
+    users.find().toArray(function(err, results) {
+        if(err) {
+            console.error('Error when getting all users');
+            console.error(err);
+            return callback(err);
+        }
+
+        results.sort(function(a, b) {
+            return a.username - b.username;
+        });
+
+        var students = []
+        results.forEach(function(result) {
+            if(result.admin == false) {
+                students.push(result);
+            }
+        });
+        callback(null, students);
+
     });
 }
 
