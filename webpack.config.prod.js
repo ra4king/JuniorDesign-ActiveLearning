2c2e9b7311fcf2f3f918ebe6ad85e5b1ff81c986
Roi Atalla <roi@roiatalla.com>
2017-02-28 07:47:07 -0500

    Slightly tweaked CSS and webpack manifest.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -18,6 +18,11 @@ module.exports = {
         ]
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
         new webpack.optimize.UglifyJsPlugin(),
         new webpack.optimize.CommonsChunkPlugin({
             names: 'vendor',
