0764129c9ea8e1ed430de8bd127bc44f7ac91110
Chad Turner <chadwickpturner@gmail.com>
2017-03-15 12:29:41 -0400

    Minor CSS fixes. Added welcome message to header. Changed question edit remove buttons to red. Added npm log to git ignore.

--- src/client/student/student-app.jsx
+++ src/client/student/student-app.jsx
@@ -143,7 +143,7 @@ class HeaderPanel extends React.Component {
         return (
             <div id='header-panel'>
                 <img id='logo' src='images/active_learning_logo_white.png' width='175' height='75' alt='logo'/>
-                <h2 id='name'>{this.props.user ? this.props.user.username : ''}</h2>
+                <h2 id='name'>Welcome, {this.props.user ? this.props.user.username : ''}!</h2>
                 <nav id='nav-links'>
                     <form method='post'>
                         <IndexLink to='/active-learning/' className='header-nav-link' activeClassName='header-nav-link-selected'>Home</IndexLink>
